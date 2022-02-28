24a5b77de7bbb4fdafa64a7192c824e66bf5b2c1
Stewart <stewartjumbe@gmail.com>
2022-02-28 10:28:23 +0000

    got database data to display on local react page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Dropdown } from "react-bootstrap";
+import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import axios from "axios";
+
 //
 class MovieGenreRow extends React.Component {
   render() {
@@ -10,20 +18,26 @@ class MovieGenreRow extends React.Component {
   }
 }
 
-class MovieRow extends React.Component {
+//Pointing rows to data
+class SpecificMovieRow extends React.Component {
   render() {
-    const movie = this.props.movie;
+    const specific_movie = this.props.specific_movie;
 
     return (
       <tr>
         {/* Table Data being called */}
-        <td>{movie.genre} </td>
 
-        <td>{movie.title}</td>
+        <td>{specific_movie.title}</td>
 
-        <td>{movie.star_rating}</td>
+        <td>{specific_movie.genre} </td>
 
-        <td>{movie.release_year}</td>
+        <td>{specific_movie.user_review}</td>
+
+        <td>{specific_movie.star_rating}</td>
+
+        <td>{specific_movie.release_year}</td>
+
+        <td>{specific_movie.length}</td>
       </tr>
     );
   }
@@ -31,17 +45,24 @@ class MovieRow extends React.Component {
 
 //Creating movie table
 class MovieTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { filmPackages: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:8080/home/All_Films")
+      .then((response) => this.setState({ filmPackages: response.data }));
+  }
+
   render() {
+    const movie = this.state.filmPackages;
+
     const rows = [];
-    let lastGenre = null;
-
-    this.props.movies.forEach((movie) => {
-      if (movie.genre !== lastGenre) {
-        rows.push(<MovieGenreRow genre={movie.genre} key={movie.genre} />);
-      }
-      rows.push(<MovieRow movie={movie} key={movie.genre} />);
-
-      lastGenre = movie.genre;
+    this.state.filmPackages.forEach((movie) => {
+      console.log(movie);
+      rows.push(<MovieRow movieinfo={movie} key={movie.film_id} />);
     });
 
     return (
@@ -49,12 +70,11 @@ class MovieTable extends React.Component {
         <thead>
           <tr>
             <th> Genre </th>
-
             <th> Title</th>
-
             <th> Rating</th>
-
             <th> Release Year</th>
+            <th> Length (min)</th>
+            <th> Film ID</th>
           </tr>
         </thead>
         <tbody>{rows}</tbody>
@@ -63,62 +83,119 @@ class MovieTable extends React.Component {
   }
 }
 
+class MovieRow extends React.Component {
+  render() {
+    const moviedata = this.props.movieinfo;
+
+    return (
+      <tr>
+        {/* sada */}
+        {/* Table Data being called */}
+        <td>{moviedata.description} </td>
+
+        <td>{moviedata.title}</td>
+
+        <td>{moviedata.star_rating}</td>
+
+        <td>{moviedata.release_year}</td>
+
+        <td>{moviedata.length}</td>
+        <td>{moviedata.film_id}</td>
+      </tr>
+    );
+  }
+}
+
+//Specific Movie Table setup
+class SpecificMovieTable extends React.Component {
+  render() {
+    const rows = [];
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th> Title</th>
+            <th> Genre </th>
+            <th> User Review</th>
+            <th> Rating</th>
+            <th> Release Year</th>
+            <th> Length</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
+//class SiteNavigation
+class SiteNavigation extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button variant="primary">Add Film</Button>{" "}
+        <Button variant="secondary">Update Review</Button>{" "}
+        <Button variant="success">Delete Review</Button>{" "}
+      </div>
+    );
+  }
+}
 //Creating the search bar
 class SearchBar extends React.Component {
   render() {
     return (
       <form>
         <input type="text" placeholder="Search Movie..." />
-        <p>
-          Choose Category:{" "}
-          <select name="category" id="category">
-            <option value="" selected="selected">
-              Action
-            </option>
-            <option value="" selected="selected">
-              Fantasy
-            </option>
-            <option value="" selected="selected">
-              Other
-            </option>
-            <option value="" selected="selected">
-              Drama
-            </option>
-          </select>
-        </p>
 
-        <p>
-          Choose Rating:{" "}
-          <select name="rating" id="rating">
-            <option value="" selected="selected">
-              5 Star
-            </option>
-            <option value="" selected="selected">
-              4 Star
-            </option>
-            <option value="" selected="selected">
-              3 Star
-            </option>
-            <option value="" selected="selected">
-              2 Star
-            </option>
-            <option value="" selected="selected">
-              1 Star
-            </option>
-          </select>
-        </p>
+        <div className="Category Dropdown">
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Choose Category
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-1">Fantasy</Dropdown.Item>
+              <Dropdown.Item href="#/action-1">Drama</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <br></br>
+
+        <div className="Rating Dropdown">
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Choose Rating
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">5 stars</Dropdown.Item>
+              <Dropdown.Item href="#/action-1">4 stars</Dropdown.Item>
+              <Dropdown.Item href="#/action-1">3 stars</Dropdown.Item>
+              <Dropdown.Item href="#/action-1">2 stars</Dropdown.Item>
+              <Dropdown.Item href="#/action-1">1 stars</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
       </form>
     );
   }
 }
 
-class FilterableMovieTable extends React.Component {
+class App extends React.Component {
   render() {
     return (
-      <div>
-        <SearchBar />
-        <MovieTable movies={this.props.movies} />
-      </div>
+      <container fluid>
+        <div>
+          <SearchBar />
+          <MovieTable movies={this.props.movies} />
+          <br></br>
+          <SiteNavigation />
+          <br></br>
+          {/* <SpecificMovieTable specific_movie={this.props.specific_movie} /> */}
+        </div>
+      </container>
     );
   }
 }
@@ -130,6 +207,7 @@ const SPECIFICMOVIE = [
     star_rating: "5",
     genre: "Action",
     release_year: "2006",
+    length: "120",
   },
 ];
 
@@ -139,6 +217,7 @@ const MOVIES = [
     title: "ACADEMY DINOSAUR",
     star_rating: "5",
     release_year: "2006",
+    length: "140",
   },
 
   {
@@ -146,6 +225,7 @@ const MOVIES = [
     title: "BRINGING HYSTERICAL ",
     star_rating: "1",
     release_year: "2006",
+    length: "150",
   },
 
   {
@@ -153,6 +233,7 @@ const MOVIES = [
     title: "ACE GOLDFINGER",
     star_rating: "2",
     release_year: "2006",
+    length: "120",
   },
 
   {
@@ -160,6 +241,7 @@ const MOVIES = [
     title: "ADAPTATION HOLES",
     star_rating: "3",
     release_year: "2006",
+    length: "130",
   },
 
   {
@@ -167,10 +249,9 @@ const MOVIES = [
     title: "AFFAIR PREJUDICE",
     star_rating: "5",
     release_year: "2006",
+    length: "160",
   },
 ];
 
-ReactDOM.render(
-  <FilterableMovieTable movies={MOVIES} />,
-  document.getElementById("root")
-);
+//Renders the FiterableMovieTable
+ReactDOM.render(<App movies={MOVIES} />, document.getElementById("root"));
